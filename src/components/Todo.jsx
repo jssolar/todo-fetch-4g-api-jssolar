e5ef64de5d07a05b7adc1300bdf20b2377d80adc
Juan Solar <juansolarsolar@yahoo.com>
2023-10-16 03:22:48 -0300

    todo-list-with-fetch-done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,88 +1,154 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/Todo.css'
 import { AiOutlineEdit } from 'react-icons/ai';
 import { RiDeleteBin7Fill } from 'react-icons/ri';
 
-
-
-
-
 const Todo = () => {
-const [text, setText] = useState('')
-const [task, setTask] = useState([])
+  // const url = 'http://localhost:3500'
+  const url = 'https://playground.4geeks.com/apis/fake/todos/user/jsolar'
 
-const handleSubmit = (e)=>{
-  e.preventDefault()
-  if (text.trim() !== "") {
-    setTask([...task, text]);
-    setText("");
-  } 
-  else{alert("Please enter a task")}
+  const [text, setText] = useState('')
+  const [task, setTask] = useState([])
+  const [user, setUser] = useState('') // Cambiado de null a ''
 
+  const getTask = () => {
+    fetch(`${url}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTask([...data])
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
   }
-const handleChange=(e)=>{
-  setText(e.target.value)
 
-}
-
-const handleEdit = (index, updatedText) => {
-  const newTask = [...task];
-  newTask[index] = updatedText;
-  setTask(newTask);
-};
+  useEffect(() => {
+      getTask()
+  }, [])
 
 
 
 
-const handleDelete = (index) => {
-  const newTask = task.filter((_, i) => i !== index);
-  setTask(newTask);
-};
 
-const handleDeleteAll = () => {
-  setTask([]);
-};
+  const handleSubmitTask = (e) => {
+    e.preventDefault()
+    if (text.trim() !== "") {
+      fetch(`${url}`, {
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(    [
+          { label: text, done: false }
+        ])
+      }).then((response) => {
+        // const newTodo = [...task, text]
+        const newTodo = [...task, { label: text, done: false }];
+        setText('')
+        setTask([...newTodo]);
+        console.log(response)
+      })
+      .catch((error) => {
+        console.error('Error al agregar una tarea:', error);
+      });
+    } else {
+      alert('Por favor, ingresa una tarea');
+    }
+  };
 
+  const handleSubmitUser = (e) => {
+    e.preventDefault()
+    if (user.trim() !== "") { // Cambiado de text a user
+      fetch(`${url}`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([])
+      }).then((response) => {
+        console.log('response', response.statusText)
+        alert('USUARIO CREADO')
+        setUser('')
+      })
+      .catch((error) => {
+        console.error('Error al agregar un usuario:', error);
+      });
+    } else {
+      alert('Por favor, ingresa un nuevo usuario');
+    }
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const handleChangeUser = (e) => {
+    setUser(e.target.value) // Cambiado de setUser a setUser
+  }
+
+  const handleEdit = (index, updatedText) => {
+    const newTask = [...task];
+    newTask[index] = updatedText;
+    setTask(newTask);
+  };
+
+  const handleDelete = (index) => {
+    const newTask = task.filter((_, i) => i !== index);
+    setTask(newTask);
+  };
+
+  
+  const handleDeleteAll = () => {
+    fetch(`${url}`, {
+      method: 'DELETE',
+    });
+    setTask([]);
+  };
 
   return (
-    <div className='container contenedor-todo'>
-      <div className="container col-md-8 ">
-        <form className="fomr-control container" onSubmit={handleSubmit}>
-          <input className='input-texto container' type="text" name="input" value={text}  onChange={handleChange} placeholder="Add a todo"/>
+    <>
+      <div className='container contenedor-todo'>
+        <div className="container col-md-8">
+          <form className="form-control container" onSubmit={handleSubmitTask}>
+            <input className='input-texto container' type="text" name="input" value={text} onChange={handleChange} placeholder="Agregar una tarea" />
+            <ul className="list-group-item container d-flex flex-column align-content-end">
+              {
+                task.map((tarea, index) => {
+                  return (
+                    <div className='contenedor-lista-tarea d-flex' key={index}>
+                      <li className="list-group container pe-0">{tarea.label}</li>
+                      <AiOutlineEdit className='boton-editar'
+                        onClick={() => {
+                          const updatedText = prompt('Editar la tarea:', tarea.label);
+                          if (updatedText !== null) {
+                            handleEdit(index, updatedText);
+                          }
+                        }}
+                      />
+                      <RiDeleteBin7Fill className="boton-eliminar" onClick={() => handleDelete(index)} />
+                    </div>
+                  )
+                })
+              }
+            </ul>
+            <button className='btn btn-info boton-submit container mt-2'type="submit">Submit</button>
+          </form>
+        </div>
+        <div className="col-md-8 container mt-5 mb-3 ">
           
-          <ul className="list-group-item container d-flex flex-column  align-content-end ">
-            {
-            task.map((tarea, index)=>{
-              return(
-              <div className='contenedor-lista-tarea d-flex ' key={index} >
-              
-                <li className="list-group container pe-0">{tarea}</li>
-                
-                <AiOutlineEdit className='boton-editar'
-                  onClick={() => {
-                    const updatedText = prompt('Edit the task:', tarea);
-                    if (updatedText !== null) {
-                      handleEdit(index, updatedText);
-                    }
-                  }}
-                />
-                <RiDeleteBin7Fill className="boton-eliminar"  onClick={() => handleDelete(index)}/>
-              </div>
-                )
-            })
-          }
-          </ul>
-          <button className='btn btn-info boton-submit container mt-2'>Submit</button>
-        </form>
-          <button
-            className='btn btn-primary elimitar-todos container  mt-2'
-            onClick={handleDeleteAll}
+          <form className="form-control bg-dark " onSubmit={handleSubmitUser}>
+            <input className='input-texto container' type="text" name="user" value={user} onChange={handleChangeUser} placeholder="Agregar un usuario" />
+            <button className='btn btn-info boton-submit container mt-2' type="submit">Crear Usuario</button>
+          </form>
+        <button
+          className='btn btn-dark eliminar-todos container mt-2'
+          onClick={handleDeleteAll}
           >
-            Delete All
-          </button>
+          Eliminar Todo
+        </button>
+          </div>
       </div>
-
-    </div>
+    </>
   )
 }
 
